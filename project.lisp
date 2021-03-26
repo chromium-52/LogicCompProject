e0cc81c48c6f2c3015421539ff8f33c8702994bc
Kenny Jung <jungdu@vdi-linux-045.ccs.neu.edu>
2021-03-26 17:17:10 -0400

    First commit

---
+++ project.lisp
@@ -0,0 +1,170 @@
+; ****************** BEGIN INITIALIZATION FOR ACL2s MODE ****************** ;
+; (Nothing to see here!  Your actual file is after this initialization code);
+(make-event
+ (er-progn
+  (set-deferred-ttag-notes t state)
+  (value '(value-triple :invisible))))
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/ccg/ccg" :uncertified-okp nil :dir :system :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
+
+;Common base theory for all modes.
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/base-theory" :dir :system :ttags :all)
+
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/custom" :dir :system :ttags :all)
+
+;; guard-checking-on is in *protected-system-state-globals* so any
+;; changes are reverted back to what they were if you try setting this
+;; with make-event. So, in order to avoid the use of progn! and trust
+;; tags (which would not have been a big deal) in custom.lisp, I
+;; decided to add this here.
+;; 
+;; How to check (f-get-global 'guard-checking-on state)
+;; (acl2::set-guard-checking :nowarn)
+(acl2::set-guard-checking :all)
+
+;Settings common to all ACL2s modes
+(acl2s-common-settings)
+;(acl2::xdoc acl2s::defunc) ;; 3 seconds is too much time to spare -- commenting out [2015-02-01 Sun]
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/acl2s-sigs" :dir :system :ttags :all)
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s mode.") (value :invisible))
+
+(acl2::xdoc acl2s::defunc) ; almost 3 seconds
+
+; Non-events:
+;(set-guard-checking :none)
+
+(set-inhibit-warnings! "Invariant-risk" "theory")
+
+(in-package "ACL2")
+(redef+)
+(defun print-ttag-note (val active-book-name include-bookp deferred-p state)
+  (declare (xargs :stobjs state)
+	   (ignore val active-book-name include-bookp deferred-p))
+  state)
+
+(defun print-deferred-ttag-notes-summary (state)
+  (declare (xargs :stobjs state))
+  state)
+
+(defun notify-on-defttag (val active-book-name include-bookp state)
+  (declare (xargs :stobjs state)
+	   (ignore val active-book-name include-bookp))
+  state)
+(redef-)
+
+(acl2::in-package "ACL2S")
+
+; ******************* END INITIALIZATION FOR ACL2s MODE ******************* ;
+;$ACL2s-SMode$;ACL2s
+;; Proving Japanese multiplication using ACL2s theorem prover
+
+; Encode the two integers into a format that ACL2s can understand
+;; using decimal expansion to output a pair of lists (each element in the list represents a particular digit)
+; Multiply each digit by each digit in the other list
+;; the number of multiplications is equal to (number of digits of first integer) * (number of digits of second integer)
+;; if the sum of the products of any digit is greater than or equal to 10, carry its tens digit
+; Put all the digits together
+
+; 42 * 21
+; l1...(4, 2)
+; l2...(2, 1)
+; 
+; 2 * 1 = 2 => 2
+;
+; 4 * 1 = 4
+; 2 * 2 = 4 => 4 + 4 = 8
+;
+; 4 * 2 = 8 => 8
+;
+; => 882
+
+; 14 * 23
+; l1...(1, 4)
+; l2...(2, 3)
+;
+; 4 * 3 = 12 => 2 (with carry of 1)
+;
+; 1 * 3 = 3
+; 4 * 2 = 8 => 1 + 3 + 8 = 2 (with carry of 1)
+;
+; 1 * 2 = 2 => 1 + 2 = 3
+;
+; => 322
+
+; 321 * 13
+; l1...(3, 2, 1)
+; l2......(1, 3)
+;
+; 1 * 3 = 3
+;
+; 2 * 3 = 6
+; 1 * 1 = 1 => 6 + 1 = 7
+;
+; 3 * 3 = 9
+; 2 * 1 = 2 => 9 + 2 = 1 (with carry of 1)
+;
+; 3 * 1 = 3 => 1 + 3 = 4
+;
+; => 4173
+
+(defdata loi (listof int))
+(defdata maybe-nat (oneof nat nil))#|ACL2s-ToDo-Line|#
+
+
+;; convert the given integer to a list with each element representing its corresponding digit
+(definec int-to-list (n :int) :tl
+  )
+
+;; multiplies elements whose indicies that add up to the magnitude equal to that of 10 raised
+;; to a power n + 2 (return a single number)
+(definec multiply (l1 :loi l2 :loi index :int) :int
+  )
+
+;; run multiply recursively and add the results of multiplying to their correct places
+(definec placeholder (l1 :loi l2 :loi) :loi
+  ; calls multiply()
+  
+  )
+
+;; process all carries
+(definec carry (l :loi) :loi
+  )
+
+;; zero-based indexOf (return nil if the element does not appear in the list)
+(definec index-of (l :loi n :int) :maybe-nat
+  (cond
+   ((endp l) 10000000000)
+   (if (equal (car l) n)
+     0
+     (1+ (index-of (cdr l) n)))))
+
+;; get the element at the given index (int-at, similar to charAt for strings)
+(definec get (l :loi index :nat) :int
+  )
+
+;(definec japanese-mult-acc (i1 :int i2 :int acc :int) :int
+;  )
+
+;; main function
+(definec japanese-mult (i1 :int i2 :int) :int
+  ; (local (l1 = int-to-list(i1)
+  ;         l2 = int-to-list(i2)))
+  ; carry(placeholder(l1, l2))
+  )
+
+
+
+
+
+
+
+
+
+
---
+++ test.lisp
@@ -0,0 +1,97 @@
+; ****************** BEGIN INITIALIZATION FOR ACL2s MODE ****************** ;
+; (Nothing to see here!  Your actual file is after this initialization code);
+(make-event
+ (er-progn
+  (set-deferred-ttag-notes t state)
+  (value '(value-triple :invisible))))
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/ccg/ccg" :uncertified-okp nil :dir :system :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
+
+;Common base theory for all modes.
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/base-theory" :dir :system :ttags :all)
+
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/custom" :dir :system :ttags :all)
+
+;; guard-checking-on is in *protected-system-state-globals* so any
+;; changes are reverted back to what they were if you try setting this
+;; with make-event. So, in order to avoid the use of progn! and trust
+;; tags (which would not have been a big deal) in custom.lisp, I
+;; decided to add this here.
+;; 
+;; How to check (f-get-global 'guard-checking-on state)
+;; (acl2::set-guard-checking :nowarn)
+(acl2::set-guard-checking :all)
+
+;Settings common to all ACL2s modes
+(acl2s-common-settings)
+;(acl2::xdoc acl2s::defunc) ;; 3 seconds is too much time to spare -- commenting out [2015-02-01 Sun]
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/acl2s-sigs" :dir :system :ttags :all)
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s mode.") (value :invisible))
+
+(acl2::xdoc acl2s::defunc) ; almost 3 seconds
+
+; Non-events:
+;(set-guard-checking :none)
+
+(set-inhibit-warnings! "Invariant-risk" "theory")
+
+(in-package "ACL2")
+(redef+)
+(defun print-ttag-note (val active-book-name include-bookp deferred-p state)
+  (declare (xargs :stobjs state)
+	   (ignore val active-book-name include-bookp deferred-p))
+  state)
+
+(defun print-deferred-ttag-notes-summary (state)
+  (declare (xargs :stobjs state))
+  state)
+
+(defun notify-on-defttag (val active-book-name include-bookp state)
+  (declare (xargs :stobjs state)
+	   (ignore val active-book-name include-bookp))
+  state)
+(redef-)
+
+(acl2::in-package "ACL2S")
+
+; ******************* END INITIALIZATION FOR ACL2s MODE ******************* ;
+;$ACL2s-SMode$;ACL2s
+(definec multiply2 (x :nat y :nat) :nat
+  (if (zp x)
+      0
+    (if (evenp x)
+(* 2 (multiply2 (floor x 2) y))
+      (+ y (* 2 (multiply2 (floor x 2) y))))))
+
+(definec multiply (x :nat y :nat) :nat
+   (if (zp x)
+       0
+     (if (evenp x)
+(multiply (floor x 2) (* y 2))
+       (+ y (multiply (floor x 2) (* y 2))))))
+
+(defthm equivalent-algorithms2
+ (implies (and (natp x) (natp y))
+   (equal (multiply x y) (* x y))))
+
+(defthm also-equivalent-algorithms2
+ (implies (and (natp x) (natp y))
+   (equal (multiply2 x y) (* x y))))#|ACL2s-ToDo-Line|#
+
+
+
+(1, 2, 3) (4, 5, 6)
+((1, 4), (2, 5), (3, 6))...a
+
+(3, 2, 1) (6, 5, 4)
+((3, 6), (2, 5), (1, 4))...b
+
+(equal (reverse (zip tl1 tl2))
+       (zip (reverse tl1) (reverse tl2)))
